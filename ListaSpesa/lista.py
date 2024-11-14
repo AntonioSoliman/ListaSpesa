96d03873b2145028de5e041ed754ab8c83925dbb
Antonio Soliman <soliman.antonio@iisgalvanimi.edu.it>
2024-11-14 07:35:27 +0000

    aggiungere elemententi alla lista

---
+++ ListaSpesa/lista.py
@@ -0,0 +1,20 @@
+lista = []
+def aggiungi():
+     x= input("Aggiungi elemento : ")
+     lista.append(x)
+
+
+
+while True:
+    print("1. Aggiungi elementi alla lista ")
+    a= int(input())
+    
+    if a== 1:
+        aggiungi()
+   
+
+
+
+
+
+print(lista)
