14f75934320348e20a4f341a83267217201900bd
Antonio Soliman <soliman.antonio@iisgalvanimi.edu.it>
2024-11-26 08:20:42 +0000

    Configurazione del DB

--- FlaskLista/app.py
+++ FlaskLista/app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+
+#configurazione db
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://lista_spesa.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# inizializza SQLAlchemy
+db.init_app(app)
+
+#crea il database se non esiste
+with app.app_context():
+    db.create_all()
 
 # Lista della spesa
 lista_spesa = []
